f2076b0f21c3bf355c23c4ec3caf5aa2500909bb
SunriseHirame <jesse.miettinen92@gmail.com>
2015-10-14 19:49:43 +0300

    asd
    asd

---
+++ Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetIndicator : MonoBehaviour {
+
+	Transform player;
+	bool show = false;
+	public float defaultColor;
+	public float damageColor;
+
+	GameObject targetMarker;
+
+	void Start () {
+		targetMarker = transform.GetChild (0).gameObject;
+		targetMarker.SetActive (false);
+	}
+
+	void Update () {
+		if (show) {
+			if (player != null) {
+				transform.LookAt (player.position);
+			} else {
+				player = GameObject.FindWithTag ("Player").transform;
+			}
+		}
+	}
+
+	void ShowTargeter () {
+		targetMarker.SetActive (true);
+		show = true;
+	}
+
+	void HideTargeter () {
+		targetMarker.SetActive (false);
+		show = false;
+	}
+}
